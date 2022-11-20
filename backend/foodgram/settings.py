779a942125cc5d0e0b26c5b9f90d42e89b76f784
Владислав Смаилов <vladislav.smailov@yandex.ru>
2022-11-20 18:24:45 +0600

    Deploy. Settings fixed

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+from dotenv import load_dotenv
+
+env_path = os.path.dirname('../infra')
+load_dotenv(dotenv_path=env_path)
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
